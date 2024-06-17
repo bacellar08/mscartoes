8e5924973e15d9b9c7fe8c47cb5006fd5f536b4c
Alexandre Bacellar <alexandre.bacellar@gmail.com>
2024-06-17 12:29:23 -0300

    feat: implementação da entide e primeiras requisições

--- src/main/java/io/github/bacellar08/mscartoes/application/CartaoController.java
+++ src/main/java/io/github/bacellar08/mscartoes/application/CartaoController.java
@@ -5,11 +5,7 @@ import io.github.bacellar08.mscartoes.domain.dto.CartaoDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -25,6 +21,7 @@ public class CartaoController {
         return "ok";
     }
 
+    @PostMapping
     public ResponseEntity saveCartao(@RequestBody CartaoDTO request) {
 
         Cartao novoCartao = request.toModel();
